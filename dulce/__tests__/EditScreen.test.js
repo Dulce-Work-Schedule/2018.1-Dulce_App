4d911c71e2c12e9e34eacfc9c575ef67219d33eb
Beatriz Hanae <beatriz.hanae@gmail.com>
2018-03-31 19:31:03 -0300

    #68 Testando snapshot.
    Signed-off-by: Beatriz Hanae <beatriz.hanae@gmail.com>
    Signed-off-by: Vitor Leal <vitinho2201@gmail.com>
    
    Co-Authored-by: Beatriz hanae <beatriz.hanae@gmail.com>
    Co-Authored-by: Vitor Leal <vitinho2201@gmail.com>

--- dulce/__tests__/EditScreen.test.js
+++ dulce/__tests__/EditScreen.test.js
@@ -2,4 +2,7 @@ import React from 'react';
 import EditScreen from '../src/Screns/EditScreen';
 import renderer from 'react-test-renderer';
 
-test('Maria Aparecida' ,() => {})
+it ('renders correctly', () => {
+  const tree = renderer.create(<EditScreen />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
