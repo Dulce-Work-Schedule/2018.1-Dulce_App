a579c6c35145d706d8badff60745f39f2e00f9c8
Guilherme Deusdará <guibanci@gmail.com>
2018-04-01 19:41:21 -0300

    HOT FIX #60 preparando para o merge/ mudando nome de rota para ingles
    Signed-off-by: Guilherme Deusdará <guibanci@gmail.com>

--- src/Screens/NewManager.js
+++ src/Screens/NewManager.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import {Text, View, TextInput, Image, Alert, ScrollView} from 'react-native';
+import {Text, View, TextInput, Image, Alert, ScrollView } from 'react-native';
 import ValidationComponent from 'react-native-form-validator';
 import AGRButton from '../Components/AGRButton';
+
 
 const logo = require('../../assets/img/logo.png');
 
@@ -18,22 +19,22 @@ const styles = {
     flexDirection: 'column',
     padding: 15,
     backgroundColor: '#FFF',
-    paddingHorizontal: 50
+    paddingHorizontal: 50,
 
   },
   input: {
     height: 36,
     borderBottomWidth: 1,
     borderBottomColor: 'grey',
+    marginBottom: 10,
     width: '100%',
     alignSelf: 'center',
     marginBottom: 20,
-    underlineColorAndroid: 'transparent'
   },
   button: {
-    marginTop: 30
+    marginTop : 30
   },
-  error: {
+  error:{
     color: 'red',
     alignSelf: 'center'
   }
@@ -50,55 +51,61 @@ export default class NewManager extends ValidationComponent {
       setor: '',
       senha: '',
       confirmaSenha: ''
-    };
+    }
   }
 
-  _onPressButton() {
-    if (this.validate({
-      nome: {required: true},
-      matricula: {numbers: true, required: true},
-      hospital: {required: true},
-      setor: {required: true},
-      senha: {minlength: 4, maxlength: 8, required: true},
-      confirmaSenha: {required: true}
-    }) && (this.state.confirmaSenha === this.state.senha)) {
-      //Metodo para mandar para a API estará aqui
-      Alert.alert(
-        'Dados do novo gestor',
-        'nome: ' + this.state.nome +
+  _onPressButton(){
+    if(this.validate({
+        nome: {required: true},
+        matricula: {numbers: true, required: true},
+        hospital: {required: true},
+        setor: {required: true},
+        senha: {minlength:4, maxlength:8, required: true},
+        confirmaSenha: {required: true}
+      }) && (this.state.confirmaSenha == this.state.senha)){
+        //Metodo para mandar para a API estará aqui
+        Alert.alert(
+          'Dados do novo gestor',
+          'nome: ' + this.state.nome +
           '\nMatricula: ' + this.state.matricula +
           '\nHospital: ' + this.state.hospital +
           '\nSetor: ' + this.state.setor +
           '\nSenha: ' + this.state.senha +
           '\nConfirmação de Senha: ' + this.state.confirmaSenha,
-        [
-          {text: 'Ok', onPress: () => this.props.navigation.navigate('login')}
-        ]
-      );
+          [
+            {text:'Ok', onPress: () => this.props.navigation.navigate('login')}
+          ]
+         )
+      }
+  }
+
+  confirmaSenhaErrado(){
+    if (this.state.confirmaSenha != this.state.senha) {
+      return true;
+    }else {
+      return false;
     }
+
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Image source={logo} style={styles.logo} />
+        <Image source={logo}  style={styles.logo} />
         <ScrollView>
-          <TextInput ref='nome' style={styles.input} placeholder='Nome Completo' onChangeText={(text) => this.setState({nome: text})}/>
-          {this.isFieldInError('nome') && this.getErrorsInField('nome').map(errorMessage => <Text style={styles.error}>{errorMessage}</Text>)}
-          <TextInput ref='matricula' style={styles.input} placeholder='Matrícula'onChangeText={(text) => this.setState({matricula: text})}/>
-          {this.isFieldInError('matricula') && this.getErrorsInField('matricula').map(errorMessage => <Text style={styles.error}>{errorMessage}</Text>)}
-          <TextInput ref='hospital' style={styles.input} placeholder='Hospital' onChangeText={(text) => this.setState({hospital: text})}/>
-          {this.isFieldInError('hospital') && this.getErrorsInField('hospital').map(errorMessage => <Text style={styles.error}>{errorMessage}</Text>)}
-          <TextInput ref='setor' style={styles.input} placeholder='Setor' onChangeText={(text) => this.setState({setor: text})}/>
-          {this.isFieldInError('setor') && this.getErrorsInField('setor').map(errorMessage => <Text style={styles.error}>{errorMessage}</Text>)}
-          <TextInput ref='senha' style={styles.input} placeholder='Senha' secureTextEntry onChangeText={(text) => this.setState({senha: text})}/>
-          {this.isFieldInError('senha') && this.getErrorsInField('senha').map(errorMessage => <Text style={styles.error}>{errorMessage}</Text>)}
-          <TextInput ref='confirmaSenha' style={styles.input} placeholder='Confirme sua senha' secureTextEntry onChangeText={(text) => this.setState({confirmaSenha: text})}/>
-          {this.state.confirmaSenha !== this.state.senha ? (
-            <Text style={styles.error}>Password and password confirmation must match</Text>
-            ) : (this.isFieldInError('confirmaSenha') && this.getErrorsInField('confirmaSenha').map(errorMessage => <Text style={styles.error}>{errorMessage}</Text>)
-          )}
-          <AGRButton style={styles.button} text='Criar' onPress={() => this._onPressButton()} />
+          <TextInput ref="nome" style={styles.input} placeholder="Nome Completo" underlineColorAndroid="transparent" onChangeText={(text) => this.setState({ nome: text})} />
+          {this.isFieldInError('nome') && this.getErrorsInField('nome').map(errorMessage => <Text style={styles.error}>{errorMessage}</Text>) }
+          <TextInput  ref="matricula"  style={styles.input} placeholder="Matrícula" underlineColorAndroid="transparent"  onChangeText={(text) => this.setState({  matricula: text})} />
+          {this.isFieldInError('matricula') && this.getErrorsInField('matricula').map(errorMessage => <Text style={styles.error}>{errorMessage}</Text>) }
+          <TextInput  ref="hospital"  style={styles.input}  placeholder="Hospital"  underlineColorAndroid="transparent"  onChangeText={(text) => this.setState({  hospital: text})}  />
+          {this.isFieldInError('hospital') && this.getErrorsInField('hospital').map(errorMessage => <Text style={styles.error}>{errorMessage}</Text>) }
+          <TextInput  ref="setor"   style={styles.input}  placeholder="Setor"  underlineColorAndroid="transparent"  onChangeText={(text) => this.setState({  setor: text})} />
+          {this.isFieldInError('setor') && this.getErrorsInField('setor').map(errorMessage => <Text style={styles.error}>{errorMessage}</Text>) }
+          <TextInput ref="senha" style={styles.input} placeholder="Senha" underlineColorAndroid="transparent" secureTextEntry onChangeText={(text) => this.setState({ senha: text})} />
+          {this.isFieldInError('senha') && this.getErrorsInField('senha').map(errorMessage => <Text style={styles.error}>{errorMessage}</Text>) }
+          <TextInput ref="confirmaSenha" style={styles.input} placeholder="Confirme sua senha" underlineColorAndroid="transparent" secureTextEntry onChangeText={(text) => this.setState({ confirmaSenha: text})} />
+          {this.isFieldInError('confirmaSenha') && this.getErrorsInField('confirmaSenha').map(errorMessage => <Text style={styles.error}>{errorMessage}</Text>) }
+          <AGRButton style={styles.button} text= 'Criar' onPress={() => this._onPressButton()} />
         </ScrollView>
       </View>
     );
