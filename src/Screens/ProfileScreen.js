643dc3b43ff5dce1988185d5e9cfbaf8bf58070c
Beatriz Hanae <beatriz.hanae@gmail.com>
2018-04-03 23:59:17 -0300

    #125 Acessando a tela de profile do funcionário a partir da lista de funcionários.
    Signed-off-by: Beatriz Hanae <beatriz.hanae@gmail.com>
    Signed-off-by: Ezequiel de Oliveira <ezequiel1de1oliveira@gmail.com>
    
    Co-Authored-by: Beatriz Hanae <beatriz.hanae@gmail.com>
    Co-Authored-by: Ezequiel de Oliveira <ezequiel1de1oliveira@gmail.com>

---
+++ src/Screens/ProfileScreen.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const styles = {
+  text: {
+    fontSize: 25,
+    alignSelf: 'center',
+    color: '#666666',
+    margin: 15
+  }
+}
+
+class ProfileScreen extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      collaborator: {},
+      loading: true
+    }
+  }
+
+  componentDidMount(){
+
+    this.setState({loading: true});
+
+    const url = 'https://jsonplaceholder.typicode.com/users/' + this.props.navigation.state.params.userId;
+
+    fetch(url)
+
+    .then((response) => {return response.json();})
+    .then((json) => {this.setState({collaborator: json,loading: false});})
+}
+    render() {
+      return this.state.loading ? (<View />) : (
+
+        <View>
+        <Text style = {styles.text}>Nome: {this.state.collaborator.name}</Text>
+        <Text style = {styles.text}>CRM/Matricula: {this.state.collaborator.address.geo.lng}</Text>
+        <Text style = {styles.text}>Setor: {this.state.collaborator.address.suite}</Text>
+        <Text style = {styles.text}>Especialidade: {this.state.collaborator.address.city}</Text>
+        <Text style = {styles.text}>Hospital: {this.state.collaborator.address.city}</Text>
+        </View>
+      )
+
+    }
+
+
+  }
+
+
+export default ProfileScreen;
