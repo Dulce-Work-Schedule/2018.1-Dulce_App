7470e4d23f2d0fa76585941888a91bb569560cda
Ezequiel de Oliveira <ezequiel1de1oliveira@gmail.com>
2018-04-04 18:09:19 -0300

    #119 Iniciando a estrutura básica do EmployeeProfileScreen.
    Signed-off-by: Ezequiel de Oliveira <ezequiel1de1oliveira@gmail.com>
    Signed-off-by: Beatriz Hanae <beatriz.hanae@gmail.com>
    
    Co-Authored-By: Ezequiel de Oliveira  <ezequiel1de1oliveira@gmail.com>
    Co-Authored-By: Beatriz Hanae <beatriz.hanae@gmail.com>

--- src/Screens/EmployeeProfileScreen.js
+++ src/Screens/EmployeeProfileScreen.js
@@ -3,21 +3,34 @@ import {View , Text} from 'react-native';
 
 class EmployeeProfileScreen extends React.native{
 
-  render(){
+  constructor(props){
+    super(props);
+    this.state = {
 
-    return(
+      employees: [],
+      loading: true
 
-      
-
-
-
-    )
+    }
 
   }
 
+  componentDidMount(){
+
+    this.setState({loading: true});
+    const url = '';
+    fetch(url)
+    .then((response) => {return response.json();})
+    .then((json) => {this.setState({employees: json,loading: false});})
+
+  }
+
+  render(){
+
+    return this.state.loading ? (<View />) : ( );
+
+  }
 }
 
-}
 
 
 
