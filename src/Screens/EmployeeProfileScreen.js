4102e1fe1ea4124d582b162668548f712882498a
Ezequiel de Oliveira <ezequiel1de1oliveira@gmail.com>
2018-04-04 13:53:50 -0300

    Signed-off-by:  Ezequiel de Oliveira  <ezequiel1de1oliveira@gmail.com> Signed-off-by: Beatriz Hanae <beatriz.hanae@gmail.com>
    Co-Authored-By: Ezequiel de Oliveira  <ezequiel1de1oliveira@gmail.com>
    Co-Authored-By: Beatriz Hanae <beatriz.hanae@gmail.com>

---
+++ src/Screens/EmployeeProfileScreen.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {View , Text} from 'react-native';
+
+class EmployeeProfileScreen extends React.native{
+
+  render(){
+
+    return(
+
+      
+
+
+
+    )
+
+  }
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default EmployeeProfileScreen;
