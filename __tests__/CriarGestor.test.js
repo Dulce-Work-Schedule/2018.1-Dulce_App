cc2afdbf1bc50ed62914bd529fceaa96d91478cb
Eliseu Egewarth <eliseuegewarth@gmail.com>
2018-04-07 14:38:45 -0300

    HOTFIX #125 excluindo teste de criar gestor
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>

--- __tests__/CriarGestor.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import CriarGestor from '../src/Screens/CriarGestor';
-
-import renderer from 'react-test-renderer';
-
-test('renders correctly', () => {
-  const tree = renderer.create(<CriarGestor />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
