07a93589d0c7522510a8958208d01e431317dbf6
Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
2018-04-10 00:18:05 -0300

    #155 Colocando botões na tela inicial
    Signed-off-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
    Signed-off-by: Vitor Leal <vitinho2201@gmail.com>
    
    Co-authored-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
    Co-authored-by: Vitor Leal <vitinho2201@gmail.com>

--- src/Screens/InitialScreen.js
+++ src/Screens/InitialScreen.js
@@ -3,23 +3,52 @@ import {
     ScrollView,
     Text,
     View,
-    Button
+    Button,
+    Image
 } from 'react-native';
 import AGRButton from '../Components/AGRButton';
+import SmallLogo from '../Components/SmallLogo';
+
+
 
 export default class InitialScreen extends React.Component {
     render() {
         return (
             <ScrollView style={{padding: 20}}>
+              <SmallLogo />
                 <Text
                     style={{fontSize: 27}}>
                     Welcome
                 </Text>
                 <View style={{margin:20}} />
                 <AGRButton
-                            onPress={() => this.props.navigation.navigate('login')}
-                            text="Logout"
-                     />
+                  onPress={() => this.props.navigation.navigate('profile')}
+                  text="Perfil"
+                />
+                <AGRButton
+                  onPress={() => this.props.navigation.navigate('edit')}
+                  text="Editar Perfil"
+                />
+                <AGRButton
+                  onPress={() => this.props.navigation.navigate('list')}
+                  text="Ver médicos"
+                />
+                <AGRButton
+                  onPress={() => this.props.navigation.navigate('newManager')}
+                  text="Criar novos gerentes"
+                />
+                <AGRButton
+                  onPress={() => this.props.navigation.navigate('EmployeeProfile')}
+                  text="Employee Profile"
+                />
+                <AGRButton
+                  onPress={() => this.props.navigation.navigate('ProfileManager')}
+                  text="Perfil gestor"
+                />
+                <AGRButton
+                  onPress={() => this.props.navigation.navigate('login')}
+                  text="Logout"
+                />
                 </ScrollView>
               );
     }
