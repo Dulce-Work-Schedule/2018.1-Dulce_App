87298f734548bd7bddf53039426ff96690b4e29d
Ezequiel de Oliveira <ezequiel1de1oliveira@gmail.com>
2018-04-13 16:19:27 -0300

    #154 tentando implementar axios no profile manager
    Signed-off-by: Ezequiel de Oliveira <ezequiel1de1oliveira@gmail.com>
    Signed-off-by: Guilherme Deusdará <guibanci@gmail.com>
    
    Co-authored-by: Ezequiel de Oliveira <ezequiel1de1oliveira@gmail.com>
    Co-authored-by: Guilherme Deusdará <guibanci@gmail.com>

--- src/Screens/NewManager.js
+++ src/Screens/NewManager.js
@@ -4,6 +4,7 @@ import ValidationComponent from 'react-native-form-validator';
 import AGRButton from '../Components/AGRButton';
 import SmallLogo from '../Components/SmallLogo';
 import AGRInput from '../Components/AGRInput';
+import axios from 'axios';
 
 
 
@@ -49,42 +50,51 @@ export default class NewManager extends ValidationComponent {
   constructor(props) {
     super(props);
     this.state = {
-      nome: '',
-      matricula: '',
+      name: '',
+      registration: '',
       hospital: '',
-      setor: '',
-      senha: '',
-      confirmaSenha: ''
+      sector: '',
+      password: '',
+      confirmepassword: ''
     };
   }
 
   _onPressButton() {
     if (this.validate({
-      nome: {required: true},
-      matricula: {numbers: true, required: true},
+      name: {required: true},
       hospital: {required: true},
-      setor: {required: true},
-      senha: {minlength: 4, maxlength: 8, required: true},
-      confirmaSenha: {required: true}
-    }) && (this.state.confirmaSenha == this.state.senha)) {
+      sector: {required: true},
+      password: {minlength: 4, maxlength: 8, required: true},
+      confirmepassword: {required: true}
+    }) && (this.state.confirmepassword == this.state.password)) {
       //Metodo para mandar para a API estará aqui
-      Alert.alert(
-        'Dados do novo gestor',
-        'nome: ' + this.state.nome +
-          '\nMatricula: ' + this.state.matricula +
-          '\nHospital: ' + this.state.hospital +
-          '\nSetor: ' + this.state.setor +
-          '\nSenha: ' + this.state.senha +
-          '\nConfirmação de Senha: ' + this.state.confirmaSenha,
-        [
-          {text: 'Ok', onPress: () => this.props.navigation.navigate('login')}
-        ]
-      );
-    }
-  }
 
-  confirmaSenhaErrado() {
-    if (this.state.confirmaSenha != this.state.senha) {
+      this.newManager();
+
+     }
+    };
+
+
+newManager(){
+
+  axios.post('http://172.17.0.1:8080/user/add', {
+      registration: this.state.registration,
+      password: this.state.password,
+      name: this.state.name,
+      hospital: this.state.hospital,
+      sector: this.state.sector,
+      manager: true
+    })
+      .then((response) => {
+       const token = response.data.token;
+       this.props.navigation.navigate('login', {token: token});
+      })
+}
+
+
+
+  confirmepasswordErrado() {
+    if (this.state.confirmepassword != this.state.password) {
       return true;
     } else {
       return false;
@@ -97,24 +107,22 @@ export default class NewManager extends ValidationComponent {
       <View style={styles.container}>
         <SmallLogo/>
         <ScrollView>
-          <AGRInput ref='nome' placeholder='Nome Completo' underlineColorAndroid='transparent'
-            onChangeText={(text) => this.setState({nome: text})} />
-          {this.isFieldInError('nome') && this.getErrorsInField('nome').map(errorMessage => <Text style={styles.error}>{errorMessage}</Text>)}
-          <AGRInput ref='matricula' placeholder='Matrícula' underlineColorAndroid='transparent'
-            onChangeText={(text) => this.setState({matricula: text})} />
-          {this.isFieldInError('matricula') && this.getErrorsInField('matricula').map(errorMessage => <Text style={styles.error}>{errorMessage}</Text>)}
-          <AGRInput ref='hospital' placeholder='Hospital' underlineColorAndroid='transparent'
-            onChangeText={(text) => this.setState({hospital: text})} />
+          <TextInput ref='name' style={styles.input} placeholder='name Completo' underlineColorAndroid='transparent' onChangeText={(text) => this.setState({name: text})} />
+          {this.isFieldInError('name') && this.getErrorsInField('name').map(errorMessage => <Text style={styles.error}>{errorMessage}</Text>)}
+          <TextInput ref='registration' style={styles.input} placeholder='Matrícula' underlineColorAndroid='transparent' onChangeText={(text) => this.setState({registration: text})} />
+          {this.isFieldInError('registration') && this.getErrorsInField('registration').map(errorMessage => <Text style={styles.error}>{errorMessage}</Text>)}
+          <TextInput ref='hospital' style={styles.input} placeholder='Hospital' underlineColorAndroid='transparent' onChangeText={(text) => this.setState({hospital: text})} />
           {this.isFieldInError('hospital') && this.getErrorsInField('hospital').map(errorMessage => <Text style={styles.error}>{errorMessage}</Text>) }
-          <AGRInput ref='setor'placeholder='Setor'underlineColorAndroid='transparent'
-          onChangeText={(text) => this.setState({setor: text})} />
-          {this.isFieldInError('setor') && this.getErrorsInField('setor').map(errorMessage => <Text style={styles.error}>{errorMessage}</Text>) }
-          <AGRInput ref='senha' placeholder='Senha'underlineColorAndroid='transparent'
-          secureTextEntry onChangeText={(text) => this.setState({senha: text})} />
-          {this.isFieldInError('senha') && this.getErrorsInField('senha').map(errorMessage => <Text style={styles.error}>{errorMessage}</Text>) }
-          <AGRInput ref='confirmaSenha' placeholder='Confirme sua senha' underlineColorAndroid='transparent'
-            secureTextEntry onChangeText={(text) => this.setState({confirmaSenha: text})} />
-          {this.isFieldInError('confirmaSenha') && this.getErrorsInField('confirmaSenha').map(errorMessage => <Text style={styles.error}>{errorMessage}</Text>) }
+          <TextInput ref='sector' style={styles.input} placeholder='sector' underlineColorAndroid='transparent' onChangeText={(text) => this.setState({sector: text})} />
+          {this.isFieldInError('sector') && this.getErrorsInField('sector').map(errorMessage => <Text style={styles.error}>{errorMessage}</Text>) }
+          <TextInput ref='password' style={styles.input} placeholder='password' underlineColorAndroid='transparent' secureTextEntry onChangeText={(text) => this.setState({password: text})} />
+          {this.isFieldInError('password') && this.getErrorsInField('password').map(errorMessage => <Text style={styles.error}>{errorMessage}</Text>) }
+          {this.isFieldInError('confirmepassword') && this.getErrorsInField('confirmepassword').map(errorMessage => <Text style={styles.error}>{errorMessage}</Text>) }
+          <TextInput ref='confirmepassword' style={styles.input}
+            placeholder='Confirme sua password'
+            underlineColorAndroid='transparent'
+            secureTextEntry
+            onChangeText={(text) => this.setState({confirmepassword: text})} />
           <AGRButton style={styles.button} text='Criar' onPress={() => this._onPressButton()} />
         </ScrollView>
       </View> );}
