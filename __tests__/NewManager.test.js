dbec2093aa7d5510bcbacd21d65e7ddeb8418dd2
Guilherme Deusdará <guibanci@gmail.com>
2018-04-17 15:37:06 -0300

    #211 Colocando o require buzier
    Signed-off-by: Guilherme Deusdará <guibanci@gmail.com>

--- __tests__/NewManager.test.js
+++ __tests__/NewManager.test.js
@@ -3,6 +3,8 @@ import NewManager from '../src/Screens/NewManager';
 
 import renderer from 'react-test-renderer';
 
+require ('bezier');
+
 test('renders correctly', () => {
   const tree = renderer.create(<NewManager />).toJSON();
   expect(tree).toMatchSnapshot();
