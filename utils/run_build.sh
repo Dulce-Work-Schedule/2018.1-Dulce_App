1daeca5d3d7b5bb84f0a7c2bfdfcff31e48b889d
Eliseu Egewarth <eliseuegewarth@gmail.com>
2018-04-18 16:00:43 -0300

    #162 Atualizando configurações do script run_build.sh
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>

--- utils/run_build.sh
+++ utils/run_build.sh
@@ -7,8 +7,16 @@ echo "MYAPP_RELEASE_STORE_PASSWORD=$MYAPP_RELEASE_STORE_PASSWORD" >> ${repo}/and
 echo "MYAPP_RELEASE_KEY_PASSWORD=$MYAPP_RELEASE_KEY_PASSWORD" >> ${repo}/android/gradle.properties && \
 ${repo}/utils/atualizar_icones.sh && \
 cd ${repo}/android/app/ && \
-${repo}/utils/decrypt.sh my-release-key.keystore.encrypt && \
-${repo}/android && \
-./gradlew assembleRelease
-
+if [ -f "my-release-key.keystore" ]
+then
+	echo -e "\033[0;31mChave Exposta\033[0m"
+else
+	${repo}/utils/decrypt.sh my-release-key.keystore.encrypt
+fi
+cd ${repo}/android && \
+if [ -z ${GRADLEW_DEBUG} ]; then
+	./gradlew assembleRelease
+else
+	./gradlew assembleDebug && ./gradlew assembleRelease
+fi
 rm "${repo}/android/gradle.properties" && mv "${repo}/android/gradle.properties.bkp" "${repo}/android/gradle.properties"
