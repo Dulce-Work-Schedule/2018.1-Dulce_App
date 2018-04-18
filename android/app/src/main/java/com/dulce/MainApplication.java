90f07fd679e320cc8179274effa3f5d761f0d5ef
Eliseu Egewarth <eliseuegewarth@gmail.com>
2018-04-18 14:09:45 -0300

    #162 Removendo import duplicado
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>

--- android/app/src/main/java/com/dulce/MainApplication.java
+++ android/app/src/main/java/com/dulce/MainApplication.java
@@ -1,7 +1,6 @@
 package com.dulce;
 
 import android.app.Application;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,7 +24,6 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new VectorIconsPackage()
-            , new VectorIconsPackage()
       );
     }
 
