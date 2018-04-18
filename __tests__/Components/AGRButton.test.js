675e2283623345020f6f1d5247eaee4db56ee488
Vitor Leal <vitinho2201@gmail.com>
2018-04-18 10:59:50 -0300

    HOTFIX #194 Adicionando testes do AGRBUTTON
    Signed-off-by: Vitor Leal <vitinho2201@gmail.com>
    Signed-off-by: Ezequiel de Oliveira <ezequiel1de1oliveira@gmail.com>
    
    Co-authored-by: Ezequiel de Oliveira  <ezequiel1de1oliveira@gmail.com>
    Co-authored-by: Vitor Leal <vitinho2201@gmail.com>

--- __tests__/Components/AGRButton.test.js
+++ __tests__/Components/AGRButton.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import AGRButton from '../../src/Components/AGRButton';
 import Adapter from 'enzyme-adapter-react-16';
+import {shallow} from 'enzyme';
 import Enzyme from 'enzyme';
 import renderer from 'react-test-renderer';
 require('bezier');
@@ -12,4 +13,12 @@ it('renders correctly', () => {
   .create(<AGRButton />)
   .toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+it('Testing onPress', () => {
+  const onPressMock = jest.fn();
+  const wrapper = shallow(<AGRButton onPress = {onPressMock}/>);
+  const button = wrapper.find('Styled(Button)').at(0);
+  button.simulate('press');
+  expect(onPressMock).toBeCalled();
 });
