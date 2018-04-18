8fcf22b6d81b9868b35f08827ed60b330183c8b2
Guilherme Deusdará <guibanci@gmail.com>
2018-04-18 17:12:19 -0300

    #223 colocano imagem com as iniciais dos medicos na lista
    Signed-off-by: Guilherme Deusdará <guibanci@gmail.com>

--- src/Screens/ListScreen.js
+++ src/Screens/ListScreen.js
@@ -3,7 +3,7 @@ import {ScrollView, Alert, FlatList} from 'react-native';
 import axios from 'axios';
 import store from '../Reducers/store';
 import {Container, Content, Spinner, ListItem, Left, Body, Thumbnail, Text} from 'native-base';
-
+import UserAvatar from 'react-native-user-avatar';
 class ListScreen extends React.Component {
 
   constructor(props) {
@@ -44,15 +44,16 @@ class ListScreen extends React.Component {
             <FlatList
               data = {this.state.employees}
               keyExtractor = {(item) => {return item._id.toString();}}
-                renderItem={(data) => {return (<ListItem onPress={() => this.props.navigation.navigate('profile', {userId: data.item._id})} key={data.item.id} avatar>
-                <Left>
-                  <Thumbnail source={{uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png'}}/>
-                </Left>
-                <Body>
-                  <Text>{data.item.name}</Text>
-                  <Text note>{data.item.registration}</Text>
-                </Body>
-            </ListItem>
+              renderItem={(data) => {return (
+                <ListItem onPress={() => this.props.navigation.navigate('profile', {userId: data.item._id})} key={data.item.id} avatar>
+                  <Left>
+                    <UserAvatar size='50' name={data.item.name} />
+                  </Left>
+                  <Body>
+                    <Text>{data.item.name}</Text>
+                    <Text note>Matrícula: {data.item.registration}</Text>
+                  </Body>
+                </ListItem>
               );
               }}
             />
