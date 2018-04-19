8ac8b1b0548342d96716c198a79653fa0ea0b290
Guilherme Deusdará <guibanci@gmail.com>
2018-04-19 01:10:59 -0300

    Fix #223 melhorando visual de perfis
    Signed-off-by: Guilherme Deusdará <guibanci@gmail.com>
    Signed-off-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
    
    Co-authored-by: Guilherme Deusdará <guibanci@gmail.com>
    Co-Authored-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>

--- src/Screens/ProfileScreen.js
+++ src/Screens/ProfileScreen.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-import ScaleIcon from '../Components/ScaleIcon';
+import {View , Text, ScrollView} from 'react-native';
 import axios from 'axios';
 import store from '../Reducers/store';
+import {Container, Content, Spinner} from 'native-base';
+import SmallLogo from '../Components/SmallLogo';
+import ScaleIcon from '../Components/ScaleIcon'
 
 const styles = {
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    padding: 20,
+    backgroundColor: '#FFF'
+
+  },
   text: {
-    fontSize: 25,
-    alignSelf: 'center',
-    color: 'black',
-    margin: 15
+    fontSize: 20,
+    marginBottom: 15,
+    alignSelf: 'center'
+  },
+  name: {
+    fontSize: 30,
+    marginBottom: 15,
+    alignSelf: 'center'
+  },
+  informacoes: {
+    alignSelf: 'center'
   },
   icon: {
     justifyContent: 'center',
@@ -21,42 +37,48 @@ class ProfileScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      collaborator: {},
+      profile: {},
       loading: true
     };
   }
 
   componentDidMount() {
-
+    this.setState({loading: true});
     const url = 'http://localhost:8080/api/userManager/listById' + this.props.navigation.state.params.userId;
-
     axios.get(url,{
-
       headers: {
         'x-access-token': store.getState().currentUser.token
       }
-
     })
-
-      .then((response) => {this.setState({collaborator: response.data,loading: false});});
+      .then((response) => {this.setState({profile: response.data,loading: false});});
   }
+
   render() {
-    return this.state.loading ? (<View />) : (
-
-      <View style = {{flex: 1}}>
-        <Text style = {styles.text}>Nome: {this.state.collaborator.name}</Text>
-        <Text style = {styles.text}>Matrícula: {this.state.collaborator.registration}</Text>
-        <Text style = {styles.text}>Hospital: {this.state.collaborator.hospital}</Text>
-        <Text style = {styles.text}>Setor: {this.state.collaborator.sector}</Text>
-
-        <View style = {styles.icon}>
-          <ScaleIcon onPress = {() => null} />
-        </View>
+    return (
+      <View style={styles.container}>
+        {
+          this.state.loading ? (
+            <Container>
+              <Content>
+                <Spinner color='purple'/>
+              </Content>
+            </Container>
+          ) : (
+            <View style={styles.informacoes}>
+            <SmallLogo />
+              <Text style={styles.name}>{this.state.profile.name}</Text>
+              <Text style={styles.text}>Matrícula:{this.state.profile.registration}</Text>
+              <Text style={styles.text}>Setor: {this.state.profile.sector}</Text>
+              <Text style={styles.text}>Hospital: {this.state.profile.hospital}</Text>
+              <View style = {styles.icon}>
+                <ScaleIcon onPress = {() => null} />
+              </View>
+            </View>
+          )
+        }
       </View>
     );
-
   }
-
 }
 
 export default ProfileScreen;
