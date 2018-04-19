646d037afb282cc8afc19afeb779c4312ce02d0e
Guilherme Deusdará <guibanci@gmail.com>
2018-04-19 01:28:04 -0300

    HOTFIX #223 tirando erros de eslint
    Signed-off-by: Guilherme Deusdará <guibanci@gmail.com>
    Signed-off-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
    
    Co-authored-by: Guilherme Deusdará <guibanci@gmail.com>
    Co-Authored-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>

--- src/Screens/InitialScreen.js
+++ src/Screens/InitialScreen.js
@@ -1,41 +1,7 @@
 import React from 'react';
-import {
-  ScrollView,
-  View,
-  TouchableOpacity,
-  StatusBar,
-  Text
-} from 'react-native';
-import SmallLogo from '../Components/SmallLogo';
 import {connect} from 'react-redux';
 import {actionLogout} from '../Actions/currentUser';
-import {NavigationActions} from 'react-navigation';
-import {Container, Content} from 'native-base';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import SideMenu from 'react-native-side-menu';
 import Router from '../Components/routes';
-
-const styles = {
-  text: {
-    marginLeft: 25
-  },
-  lastButton: {
-    marginBottom: 150
-  },
-  image: {
-    height: 30,
-    width: 30,
-    paddingLeft: 5
-  },
-  btn: {
-    marginBottom: 20,
-    paddingHorizontal: 48
-  },
-  container: {
-    backgroundColor: '#FFF'
-  }
-
-};
 
 class InitialScreen extends React.Component {
   render() {
