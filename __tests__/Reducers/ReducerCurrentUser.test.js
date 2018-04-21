e3f57c0299465038d42fe108878dcb304e5a0866
Ezequiel de Oliveira <ezequiel1de1oliveira@gmail.com>
2018-04-21 00:06:50 -0300

    HOTFIX #192 adicionando arquivos de teste para reducers
    Signed-off-by: Vitor Leal <vitinho2201@gmail.com>
    Signed-off-by: Ezequiel de Oliveira <ezequiel1de1oliveira@gmail.com>
    
    Co-authored-by: Ezequiel de Oliveira <ezequiel1de1oliveira@gmail.com>
    Co-authored-by: Vitor Leal <vitinho2201@gmail.com>

---
+++ __tests__/Reducers/ReducerCurrentUser.test.js
@@ -0,0 +1,32 @@
+import reducer from '../../src/Reducers/currentUser';
+import {LOGIN, LOGOUT} from '../../src/Actions/currentUser';
+
+const currentUser = {
+  token: 'test'
+};
+
+describe('currentUser reducer', () => {
+  it('should return the initial state',() => {
+    expect(reducer(void 0,{})).toEqual({});
+  });
+
+  it('should handle the LOGIN action', () => {
+    expect(
+      reducer(currentUser,{
+        type: LOGIN,
+        currentUser: currentUser
+      })
+    ).toEqual({
+      token: 'test'
+    });
+  });
+
+  it('should handle the LOGOUT action', () => {
+    expect(
+      reducer(currentUser,{
+        type: LOGOUT,
+        currentUser: {}
+      })
+    ).toEqual({});
+  });
+});
