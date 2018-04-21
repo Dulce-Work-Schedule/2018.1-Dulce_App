053df0ad92a1acd5bc9ce0579477a95729fecb58
Vitor Leal <vitinho2201@gmail.com>
2018-04-21 00:43:07 -0300

    HOTFIX #192 atualizando a rota de NewManager
    Signed-off-by: Vitor Leal <vitinho2201@gmail.com>
    Signed-off-by: Ezequiel de Oliveira <ezequiel1de1oliveira@gmail.com>
    
    Co-authored-by: Ezequiel de Oliveira <ezequiel1de1oliveira@gmail.com>
    Co-authored-by: Vitor Leal <vitinho2201@gmail.com>

--- __tests__/Routes/NewManager.test.js
+++ __tests__/Routes/NewManager.test.js
@@ -3,8 +3,8 @@ import renderer from 'react-test-renderer';
 import NewManager from '../../src/Routes/NewManager';
 
 test('renders correctly' , () => {
-    const tree = renderer
-    .create(<NewManager />)
-    .toJSON();
-    expect(tree).toMatchSnapshot();
-  });
+  const tree = renderer
+  .create(<NewManager />)
+  .toJSON();
+  expect(tree).toMatchSnapshot();
+});
