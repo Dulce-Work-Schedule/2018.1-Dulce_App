151297974cd357e985d8dee281eace79a7a963f8
Ezequiel de Oliveira <ezequiel1de1oliveira@gmail.com>
2018-04-21 00:11:38 -0300

    HOTFIX #192 criando arquivo de teste pra rotas
    Signed-off-by: Vitor Leal <vitinho2201@gmail.com>
    Signed-off-by: Ezequiel de Oliveira <ezequiel1de1oliveira@gmail.com>
    
    Co-authored-by: Ezequiel de Oliveira <ezequiel1de1oliveira@gmail.com>
    Co-authored-by: Vitor Leal <vitinho2201@gmail.com>

---
+++ __tests__/Routes/Doctors.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Doctors from '../../src/Routes/Doctors';
+
+test('renders correctly' , () => {
+    const tree = renderer
+    .create(<Doctors />)
+    .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
