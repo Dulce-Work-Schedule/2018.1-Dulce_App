948e803f0322e1469db88b030c011e1ffe96c6e7
Vitor Leal <vitinho2201@gmail.com>
2018-04-21 00:42:02 -0300

    HOTFIX #192 atualizando o eslint da rota de Doctors
    Signed-off-by: Vitor Leal <vitinho2201@gmail.com>
    Signed-off-by: Ezequiel de Oliveira <ezequiel1de1oliveira@gmail.com>
    
    Co-authored-by: Ezequiel de Oliveira <ezequiel1de1oliveira@gmail.com>
    Co-authored-by: Vitor Leal <vitinho2201@gmail.com>

--- __tests__/Routes/Doctors.test.js
+++ __tests__/Routes/Doctors.test.js
@@ -3,8 +3,8 @@ import renderer from 'react-test-renderer';
 import Doctors from '../../src/Routes/Doctors';
 
 test('renders correctly' , () => {
-    const tree = renderer
-    .create(<Doctors />)
-    .toJSON();
-    expect(tree).toMatchSnapshot();
-  });
+  const tree = renderer
+  .create(<Doctors />)
+  .toJSON();
+  expect(tree).toMatchSnapshot();
+});
