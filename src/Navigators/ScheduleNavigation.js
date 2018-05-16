4f49fc1275180b388577f8c31f12238d62c73bfa
Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
2018-05-15 21:57:11 -0300

    #302 Criando arquivos iniciais para tela e rotas
    Signed-off-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
    Signed-off-by: Ezequiel de Oliveira <ezequiel1de1oliveira@gmail.com>
    
    Co-authored-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
    Co-authored-by: Ezequiel de Oliveira <ezequiel1de1oliveira@gmail.com>

---
+++ src/Navigators/ScheduleNavigation.js
@@ -0,0 +1,16 @@
+import WeekSchedule from '../Screens/WeekSchedule';
+import {StackNavigator} from 'react-navigation';
+
+const routes = {
+  week: {
+    screen: WeekSchedule,
+    navigationOptions: {
+      header: null
+    }
+  };
+
+const config = {};
+
+const ScheduleNavigation = StackNavigator(routes, config);
+
+export default ScheduleNavigation;
---
+++ src/Screens/WeekSchedule.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Container, Header, Content, List, ListItem, Text, Body, Title} from 'native-base';
+
+class WeekScheduleScreen extends React.Component {
+  render(){
+    return(
+      <View>
+        <Text>
+          Aqui você verá os horários da semana
+        </Text>
+      </View>
+    );
+  }
+}
+
+export default WeekScheduleScreen;
