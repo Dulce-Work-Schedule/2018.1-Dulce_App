296cef5932ad72fd8d938fc55ad8d4528604fc39
Beatriz <beatriz.hanae@gmail.com>
2018-05-17 20:16:22 -0300

    #298 Tentando aparecer o dia quando selecionado de acordo com o protipo
    Signed-off-by: Beatriz Hanae <beatriz.hanae@gmail.com>
    Signed-off-by: Ezequiel de Oliveira <ezequiel1de1oliveira@gmail.com>

--- src/Screens/MonthScheduleScreen.js
+++ src/Screens/MonthScheduleScreen.js
@@ -12,9 +12,6 @@ const styles = {
     marginTop: 5,
     textAlign: 'center'
   },
-  calendar: {
-    justifyContent: 'center'
-  },
   header: {
     backgroundColor: '#8a2be2',
     padding: 10
@@ -25,6 +22,11 @@ const styles = {
     textAlign: 'left',
     textAlignVertical: 'center',
     marginLeft: 70
+  },
+  day: {
+    flexDirection: 'row',
+    backgroundColor: 'red',
+    justifyContent: 'flex-start'
   }
 };
 
@@ -44,13 +46,14 @@ class MonthScheduleScreen extends React.Component {
         <View style={styles.header}>
             <Text style={styles.textHeader}>Escala</Text>
         </View>
-        <Calendar style={styles.calendar}
+        <Calendar
+
           // Initially visible month. Default = Date()
           current={Date()}
           // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-          minDate={'2018-05-10'}
+          minDate={'2018-05-01'}
           // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-          maxDate={'2020-05-15'}
+          maxDate={'2020-05-31'}
           // Handler which gets executed on day press. Default = undefined
           onDayPress={(day) => {
             console.log('selected day', day)
@@ -59,7 +62,7 @@ class MonthScheduleScreen extends React.Component {
           // Handler which gets executed on day long press. Default = undefined
           onDayLongPress={(day) => {console.log('selected day', day)}}
           // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
-          monthFormat={'MM yyyy'}
+          monthFormat={'MM-yyyy'}
           // Handler which gets executed when visible month changes in calendar. Default = undefined
           onMonthChange={() => {console.log('month changed', month)}}
           // Hide month navigation arrows. Default = false
@@ -69,7 +72,7 @@ class MonthScheduleScreen extends React.Component {
           // Replace default arrows with custom ones (direction can be 'left' or 'right')
           renderArrow={(left) => (<Arrow />)}
           // Do not show days of other months in month page. Default = false
-          hideExtraDays={true}
+          hideExtraDays={false}
           // If hideArrows=false and hideExtraDays=false do not switch month when tapping on greyed out
           // day from another month that is visible in calendar page. Default = false
           disableMonthChange={true}
@@ -83,8 +86,12 @@ class MonthScheduleScreen extends React.Component {
           onPressArrowLeft={substractMonth => substractMonth()}
           // Handler which gets executed when press arrow icon left. It receive a callback can go next month
           onPressArrowRight={addMonth => addMonth()}
+
+          pastScrollRange={24}
+          futureScrollRange={24}
+        style={{borderBottomWidth: 1, borderBottomColor: 'black'}}
         />
-        <View>
+        <View style={styles.day}>
         <Text style={styles.text}>{this.state.selected_day.dateString}</Text>
         </View>
       </View>
