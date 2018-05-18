4bac370be6fb8f532a1ccfdaa36c36097d4f9565
Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
2018-05-17 22:31:12 -0300

    #302 Começando a estruturar a tela de visualização das escalas por semana
    Signed-off-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
    Signed-off-by: Ezequiel de Oliveira <ezequiel1de1oliveira@gmail.com>
    
    Co-authored-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
    Co-authored-by: Ezequiel de Oliveira <ezequiel1de1oliveira@gmail.com>

--- App.js
+++ App.js
@@ -1,18 +1,33 @@
+// import React from 'react';
+// import LoginNavigator from './src/Navigators/LoginNavigator';
+// import {Provider} from 'react-redux';
+// import store from './src/Reducers/store';
+// store.subscribe(() => {
+//   console.log(store.getState());
+// });
+//
+// class App extends React.Component {
+//
+//   render() {
+//     return (
+//       <Provider store={store}>
+//         <LoginNavigator />
+//       </Provider>
+//     );
+//   }
+// }
+//
+// export default App;
+
+
 import React from 'react';
-import LoginNavigator from './src/Navigators/LoginNavigator';
-import {Provider} from 'react-redux';
-import store from './src/Reducers/store';
-store.subscribe(() => {
-  console.log(store.getState());
-});
+import ScheduleNavigation from './src/Navigators/ScheduleNavigation';
 
 class App extends React.Component {
 
   render() {
     return (
-      <Provider store={store}>
-        <LoginNavigator />
-      </Provider>
+      <ScheduleNavigation />
     );
   }
 }
