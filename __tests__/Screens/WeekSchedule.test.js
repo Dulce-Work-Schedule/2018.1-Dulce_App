ce344f108c4452c288d67710808bd05e5fd3a208
Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
2018-05-18 21:16:10 -0300

    Hotfix #302 Arrumando teste
    Signed-off-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
    Signed-off-by: Ezequiel de Oliveira <ezequiel1de1oliveira@gmail.com>
    
    Co-authored-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
    Co-authored-by: Ezequiel de Oliveira <ezequiel1de1oliveira@gmail.com>

--- __tests__/Screens/WeekSchedule.test.js
+++ __tests__/Screens/WeekSchedule.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import WeekSchedule from '../../src/Screens/WeekSchedule';
 
+require('bezier');
+
 const props = {
 
   navigation: {
