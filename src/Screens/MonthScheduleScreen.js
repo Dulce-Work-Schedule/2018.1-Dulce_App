067cfbc8286d406b5d24c9e019166a111236ccea
Beatriz <beatriz.hanae@gmail.com>
2018-05-18 21:00:25 -0300

    #298 Arrumando a funcionalidade da seta e colocando o botão de editar a escala.
    Signed-off-by: Beatriz Hanae <beatriz.hanae@gmail.com>
    Signed-off-by: Vitor Leal <vitinho2201@gmail.com>
    
    Co-Authored-by: Beatriz Hanae <beatriz.hanae@gmail.com>
    Co-Authored-by: Vitor Leal <vitinho2201@gmail.com>

--- src/Screens/MonthScheduleScreen.js
+++ src/Screens/MonthScheduleScreen.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-//import {Calendar, Arrow} from 'react-native-calendars';
+import {View, Text, Image} from 'react-native';
 import {Calendar, Arrow} from 'react-native-calendars';
-import calendarHeader from 'react-native-calendars';
+import CalendarHeader from 'react-native-calendars';
+import {Button} from 'native-base';
+
 const styles = {
   text: {
     fontSize: 30,
-    color: '#00F' ,
-    //marginBottom: 5,
-    //marginTop: 5,
+    color: '#00F',
+    marginRight: 20,
     textAlign: 'center'
   },
   header: {
-    backgroundColor: '#8a2be2',
+    backgroundColor: '#75027a',
     padding: 10
   },
   textHeader: {
@@ -23,8 +23,15 @@ const styles = {
   },
   day: {
     flexDirection: 'row',
-    backgroundColor: 'red',
-    justifyContent: 'flex-start'
+    backgroundColor: '#ffffff'
+  },
+  button: {
+    marginLeft: 350,
+    backgroundColor: '#75027a'
+  },
+  imageEdit: {
+    width: 45,
+    height: 45
   }
 };
 
@@ -34,6 +41,9 @@ class MonthScheduleScreen extends React.Component {
     this.state = {
       selected_day: {
         dateString: ''
+      },
+      change_month: {
+        month: ''
       }
     };
   }
@@ -43,50 +53,52 @@ class MonthScheduleScreen extends React.Component {
         <View style={styles.header}>
           <Text style={styles.textHeader}>Escala</Text>
         </View>
-          <Calendar
+        <Calendar
+          // Initially visible month. Default = Date()
+          current={Date()}
+          // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
+          minDate={'2018-05-01'}
+          // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
+          maxDate={'2020-05-31'}
+          // Handler which gets executed on day press. Default = undefined
+          onDayPress={(day) => {
+            console.log('selected day', day);
+            this.setState({selected_day: day});
+          }}
+          // Handler which gets executed on day long press. Default = undefined
+          onDayLongPress={(day) => {console.log('selected day', day);}}
+          // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
+          monthFormat={'MM-yyyy'}
+          // Handler which gets executed when visible month changes in calendar. Default = undefined
+          onMonthChange={(month) => {
+            console.log('month changed', month);
+          }}
+          // Hide month navigation arrows. Default = false
+          hideArrows={false}
+          // Do not show days of other months in month page. Default = false
+          hideExtraDays={false}
+          // If hideArrows=false and hideExtraDays=false do not switch month when tapping on greyed out
+          // day from another month that is visible in calendar page. Default = false
+          disableMonthChange={false}
+          // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
+          firstDay={7}
+          // Hide day names. Default = false
+          hideDayNames={false}
+          // Show week numbers to the left. Default = false
+          showWeekNumbers={true}
+          // Handler which gets executed when press arrow icon left. It receive a callback can go back month
+          onPressArrowLeft={substractMonth => substractMonth()}
+          // Handler which gets executed when press arrow icon left. It receive a callback can go next month
+          onPressArrowRight={addMonth => addMonth()}
 
-            // Initially visible month. Default = Date()
-            current={Date()}
-            // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-            minDate={'2018-05-01'}
-            // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-            maxDate={'2020-05-31'}
-            // Handler which gets executed on day press. Default = undefined
-            onDayPress={(day) => {
-              console.log('selected day', day)
-              this.setState({selected_day: day})
-            }}
-            // Handler which gets executed on day long press. Default = undefined
-            onDayLongPress={(day) => {console.log('selected day', day)}}
-            // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
-            monthFormat={'MM-yyyy'}
-            // Handler which gets executed when visible month changes in calendar. Default = undefined
-            onMonthChange={() => {console.log('month changed', month)}}
-            // Hide month navigation arrows. Default = false
-            hideArrows={false}
-            // Do not show days of other months in month page. Default = false
-            hideExtraDays={false}
-            // If hideArrows=false and hideExtraDays=false do not switch month when tapping on greyed out
-            // day from another month that is visible in calendar page. Default = false
-            disableMonthChange={true}
-            // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
-            firstDay={7}
-            // Hide day names. Default = false
-            hideDayNames={false}
-            // Show week numbers to the left. Default = false
-            showWeekNumbers={true}
-            // Handler which gets executed when press arrow icon left. It receive a callback can go back month
-            onPressArrowLeft={substractMonth => substractMonth()}
-            // Handler which gets executed when press arrow icon left. It receive a callback can go next month
-            onPressArrowRight={addMonth => addMonth()}
-
-            pastScrollRange={24}
-            futureScrollRange={24}
+          pastScrollRange={24}
+          futureScrollRange={24}
           style={{borderBottomWidth: 1, borderBottomColor: 'black'}}
-          />
-          <View style={styles.day}>
+        />
+        <View style={styles.day}>
           <Text style={styles.text}>{this.state.selected_day.dateString}</Text>
-          </View>
+        </View>
+        <View><Button rounded light style={styles.button}><Image style={styles.imageEdit} source={require('../../assets/img/editprofile.png')} /></Button></View>
       </View>
     );
   }
