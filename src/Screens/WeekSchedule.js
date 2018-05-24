2ba5d1276a88788aa040a1606773319a07943471
Guilherme Deusdará <guibanci@gmail.com>
2018-05-24 20:42:59 -0300

    #331 refatorando funcões da tela
    Signed-off-by: Guilherme Deudará <guibanci@gmail.com>
    Signed-off-by: Ezequiel De Oliveira <ezequiel1de1oliveira@gmail.com>
    
    Co-Authored-by: Guilherme Deudará <guibanci@gmail.com>
    Co-Authored-by: Ezequiel De Oliveira  <ezequiel1de1oliveira@gmail.com>

--- src/Screens/WeekSchedule.js
+++ src/Screens/WeekSchedule.js
@@ -35,12 +35,9 @@ export default class WeekSchedule extends Component {
     super(props);
     this.state = {
       modalVisible: false,
-      currentSchedule: '',
-      selectedSchedule: '',
-      selectedStartTime: '',
-      selectedFinalTime: '',
-      currentStartTime: '',
-      currentFinalTime: '',
+      
+      currentSchedule: {},
+      selectedSchedule: {},
 
       items: {
         '2017-05-15': [
@@ -128,44 +125,44 @@ export default class WeekSchedule extends Component {
     // console.log(`Load Items for ${day.year}-${day.month}`);
   }
 
-  _alert(employee, start_time, end_time){
-    this.setState({currentSchedule: employee, currentStartTime: start_time, currentFinalTime: end_time});
+  _alert(employee) {
+    this.setState({ currentSchedule: employee });
     Alert.alert(
-    'Mudar de Horário',
-    'Deseja solicitar mudança de horário?',
-    [
-      {text: 'Sim', onPress: () => {this.setModalVisible(true)}},
-      {text: 'Não', onPress: () => {}},
-    ],
-    { cancelable: true }
-  )
+      'Mudar de Horário',
+      'Deseja solicitar mudança de horário?',
+      [
+        { text: 'Sim', onPress: () => { this.setModalVisible(true) } },
+        { text: 'Não', onPress: () => { } },
+      ],
+      { cancelable: true }
+    )
   }
 
-  alert_change(employee, start_time, end_time){
-   this.setState({selectedSchedule: employee, selectedStartTime: start_time, selectedFinalTime: end_time}, () => {
+  alert_change(employee) {
+    this.setState({ selectedSchedule: employee }, () => {
 
-    Alert.alert(
-    'Mudar de Horário',
-    this.state.currentSchedule + ': ' + this.state.currentStartTime + ' - ' + this.state.currentFinalTime + '\nPor\n' +
-    this.state.selectedSchedule + ': ' + this.state.selectedStartTime + ' - ' + this.state.selectedFinalTime + '\nDeseja solicitar mudança de horário?',
-    [
-      {text: 'Sim', onPress: () => {this.setModalVisible(false)}},
-      {text: 'Não', onPress: () => {}},
-    ],
-    { cancelable: true }
-  )
-});
-}
+      Alert.alert(
+        'Mudar de Horário',
+        this.state.currentSchedule.employee + ', deseja trocar de horario com o/a ' + this.state.selectedSchedule.employee + '?\n ' +
+        this.state.currentSchedule.start_time + ' - ' + this.state.currentSchedule.end_time + ' -> ' + this.state.selectedSchedule.start_time + ' - ' + this.state.selectedSchedule.end_time,
+        [
+          { text: 'Sim', onPress: () => { this.setModalVisible(false) } },
+          { text: 'Não', onPress: () => { } },
+        ],
+        { cancelable: true }
+      )
+    });
+  }
 
   renderItem(item) {
     console.log(item);
     return (
-      <TouchableHighlight onPress={() => {this._alert(item.employee, item.start_time, item.end_time)}} style={[styles.item, { height: item.height }]}>
-      <View>
-        <Text>{item.employee}</Text>
-        <Text>{item.specialty}</Text>
-        <Text>{item.start_time} - {item.end_time}</Text>
-        <Text>{item.amount_of_hours}</Text>
+      <TouchableHighlight onLongPress={() => { this._alert(item) }} style={[styles.item, { height: item.height }]}>
+        <View>
+          <Text>{item.employee}</Text>
+          <Text>{item.specialty}</Text>
+          <Text>{item.start_time} - {item.end_time}</Text>
+          <Text>{item.amount_of_hours}</Text>
         </View>
       </TouchableHighlight>
     );
@@ -174,41 +171,61 @@ export default class WeekSchedule extends Component {
   renderChangeItem(item) {
     console.log(item);
     return (
-      <TouchableHighlight onPress={() => {this.alert_change(item.employee, item.start_time, item.end_time)}} style={[styles.item, { height: item.height }]}>
-      <View>
-        <Text>{item.employee}</Text>
-        <Text>{item.specialty}</Text>
-        <Text>{item.start_time} - {item.end_time}</Text>
-        <Text>{item.amount_of_hours}</Text>
+      <TouchableHighlight onPress={() => { this.alert_change(item) }} style={[styles.item, { height: item.height }]}>
+        <View>
+          <Text>{item.employee}</Text>
+          <Text>{item.specialty}</Text>
+          <Text>{item.start_time} - {item.end_time}</Text>
+          <Text>{item.amount_of_hours}</Text>
         </View>
       </TouchableHighlight>
     );
   }
 
-  renderModal(){
-    return(<View>
-    <Modal isVisible={this.state.modalVisible} backdropOpacity={0.2} style={{backgroundColor:'white'}} onBackdropPress={() => {this.setModalVisible(false)}}>
-      <View style={{ flex: 1 }} >
-      <Text>Selecione o Horário que Deseja solicitar troca</Text>
-      <View style={{flex: 1}}>
+  renderModal() {
+    return (
+      <View>
+        <Modal isVisible={this.state.modalVisible} backdropOpacity={0.2} style={{ backgroundColor: 'white' }} onBackdropPress={() => { this.setModalVisible(false) }}>
+          <View style={{ flex: 1 }} >
+            <Text>Selecione o Horário que Deseja solicitar troca</Text>
+            <View style={{ flex: 1 }}>
+              {this.renderAgenda(this.renderChangeItem)}
+            </View>
+          </View>
+        </Modal>
+      </View>
+    )
+  }
+
+  renderAgenda(item) {
+    return (
       <Agenda
-        style={{flex:1}}
+        style={{ flex: 1 }}
         items={this.state.items}
         loadItemsForMonth={this.loadItems.bind(this)}
         selected={this.state.selectedDay}
-        renderItem={this.renderChangeItem.bind(this)}
+        renderItem={item.bind(this)}
         renderEmptyDate={this.renderEmptyDate.bind(this)}
         rowHasChanged={this.rowHasChanged.bind(this)}
+        //markingType={'period'}
+        // markedDates={{
+        //    '2017-05-08': {textColor: '#666'},
+        //    '2017-05-09': {textColor: '#666'},
+        //    '2017-05-14': {startingDay: true, endingDay: true, color: 'blue'},
+        //    '2017-05-21': {startingDay: true, color: 'blue'},
+        //    '2017-05-22': {endingDay: true, color: 'gray'},
+        //    '2017-05-24': {startingDay: true, color: 'gray'},
+        //    '2017-05-25': {color: 'gray'},
+        //    '2017-05-26': {endingDay: true, color: 'gray'}}}
+        //  monthFormat={'yyyy'}
+        // renderDay={(day, item) => (<Text>{day ? day.day: 'item'}</Text>)}
         theme={{
           calendarBackground: '#ffffff',
           agendaKnobColor: '#5f4b8b',
           selectedDayBackgroundColor: '#5f4b8b'
         }}
       />
-      </View>
-      </View>
-    </Modal>
-  </View>)
+    )
   }
 
   renderEmptyDate() {
@@ -227,35 +244,10 @@ export default class WeekSchedule extends Component {
   }
   render() {
     return (
-        <View style={{flex: 1}}>
-        <Agenda
-        style={{flex:1}}
-          items={this.state.items}
-          loadItemsForMonth={this.loadItems.bind(this)}
-          selected={this.state.selectedDay}
-          renderItem={this.renderItem.bind(this)}
-          renderEmptyDate={this.renderEmptyDate.bind(this)}
-          rowHasChanged={this.rowHasChanged.bind(this)}
-          //markingType={'period'}
-          // markedDates={{
-          //    '2017-05-08': {textColor: '#666'},
-          //    '2017-05-09': {textColor: '#666'},
-          //    '2017-05-14': {startingDay: true, endingDay: true, color: 'blue'},
-          //    '2017-05-21': {startingDay: true, color: 'blue'},
-          //    '2017-05-22': {endingDay: true, color: 'gray'},
-          //    '2017-05-24': {startingDay: true, color: 'gray'},
-          //    '2017-05-25': {color: 'gray'},
-          //    '2017-05-26': {endingDay: true, color: 'gray'}}}
-          //  monthFormat={'yyyy'}
-          // renderDay={(day, item) => (<Text>{day ? day.day: 'item'}</Text>)}
-          theme={{
-            calendarBackground: '#ffffff',
-            agendaKnobColor: '#5f4b8b',
-            selectedDayBackgroundColor: '#5f4b8b'
-          }}
-        />
+      <View style={{ flex: 1 }}>
+        {this.renderAgenda(this.renderItem)}
         {this.renderModal()}
-        </View>
+      </View>
     );
   }
 }
