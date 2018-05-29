bbc4d9bbfa84be3443f1bedb02d4732418025c40
Eliseu Egewarth <eliseuegewarth@gmail.com>
2018-05-29 19:48:36 -0300

    HOTFIX: #216 Adicionando alguns níveis de log no script de atualização da versão
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>

--- utils/run_update_version_code.sh
+++ utils/run_update_version_code.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
-repo=$(git rev-parse --show-toplevel) && \
-cd ${repo}/android && \
-fastlane update_version && \
-version_code=$(cat ${repo}/android/app/build.gradle | grep " versionCode "  | tr -s '[:space:]') && \
-echo "$git_credentials" > ~/.git-credentials && \
-git config credential.helper store && \
-git add ${repo}/android/app/build.gradle && \
-git commit -m"[ci skip] Updating to ${version_code}." && \
-git push origin master
+repo=$(git rev-parse --show-toplevel);
+echo "cd ${repo}/android" ;
+cd ${repo}/android;
+echo "fastlane update_version" ;
+fastlane update_version;
+echo "version_code=" ;
+version_code=$(cat ${repo}/android/app/build.gradle | grep " versionCode "  | tr -s '[:space:]');
+echo "echo git_credentials" ;
+if [ -z $git_credentials ]; then
+	echo -e "\033[0;31mEmpty env git_credentials...\033[0m";
+	exit 1;
+else
+	echo "$git_credentials" > ~/.git-credentials;
+	echo "git config credential.helper store" ;
+	git config credential.helper store;
+	echo "git add ${repo}/android/app/build.gradle" ;
+	git branch;
+	git add ${repo}/android/app/build.gradle;
+	git git log;
+	echo "git commit -m [ci skip] Updating to ${version_code}." ;
+	git commit -m"[ci skip] Updating to ${version_code}.";
+	git log;
+	echo "git push origin master";
+	git push origin master
+fi
