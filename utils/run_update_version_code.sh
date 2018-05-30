f8357f5760c03990e8a6ad85e03f7ace708f2328
Eliseu Egewarth <eliseuegewarth@gmail.com>
2018-05-29 23:08:24 -0300

    HOTFIX: #216 Adicionando configurações de autor do commit
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>
    Signed-off-by: João Egewarth <egewarth@gmail.com>
    
    Co-authored-by: João Egewarth <egewarth@gmail.com>

--- utils/run_update_version_code.sh
+++ utils/run_update_version_code.sh
@@ -1,26 +1,53 @@
 #!/bin/bash
 repo=$(git rev-parse --show-toplevel);
-echo "cd ${repo}/android" ;
-cd ${repo}/android;
-echo "fastlane update_version" ;
-fastlane update_version;
-echo "version_code=" ;
-version_code=$(cat ${repo}/android/app/build.gradle | grep " versionCode "  | tr -s '[:space:]');
-echo "echo git_credentials" ;
+if [ -z $git_user_name ]; then
+	echo -e "\033[0;31mEmpty env git_user_name...\033[0m";
+	exit 1;
+else
+	git config --local user.name "$git_user_name"
+fi
+if [ -z $git_user_email ]; then
+	echo -e "\033[0;31mEmpty env git_user_email...\033[0m";
+	exit 1;
+else
+	git config --local user.email "$git_user_email"
+fi
 if [ -z $git_credentials ]; then
 	echo -e "\033[0;31mEmpty env git_credentials...\033[0m";
 	exit 1;
 else
+	echo "echo git_credentials" ;
 	echo "$git_credentials" > ~/.git-credentials;
-	echo "git config credential.helper store" ;
-	git config credential.helper store;
-	echo "git add ${repo}/android/app/build.gradle" ;
-	git branch;
-	git add ${repo}/android/app/build.gradle;
-	git git log;
-	echo "git commit -m [ci skip] Updating to ${version_code}." ;
-	git commit -m"[ci skip] Updating to ${version_code}.";
-	git log;
-	echo "git push origin master";
-	git push origin master
 fi
+
+echo "git config credential.helper store" ;
+git config credential.helper store;
+
+echo "cd ${repo}/android" ;
+cd ${repo}/android;
+
+git checkout -b update_version;
+git branch --set-upstream-to=origin/master
+
+git branch;
+
+echo "fastlane update_version" ;
+fastlane update_version;
+
+echo "version_code=" ;
+version_code=$(cat ${repo}/android/app/build.gradle | grep " versionCode "  | tr -s '[:space:]');
+
+git branch;
+
+echo "git add ${repo}/android/app/build.gradle" ;
+git add ${repo}/android/app/build.gradle;
+
+git log -n 1;
+
+echo "git commit -m [ci skip] Updating to ${version_code}." ;
+git commit -m"[ci skip] Updating to ${version_code}.";
+
+git log -n 1;
+
+echo "git push origin master";
+git push origin master
