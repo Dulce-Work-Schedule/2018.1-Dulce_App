3dfdf655cc01a5a0ce4b830573d01281cfa7b64f
Ezequiel De Oliveira <ezequiel1de1oliveira@gmail.com>
2018-05-31 23:50:30 -0300

    #330 finalizando screen
    Signed-off-by: Vitor Leal <vitinho2201@gmail.com>
    Signed-off-by: Ezequiel de Oliveira <ezequiel1de1oliveira@gmail.com>
    
    Co-authored-by: Vitor Leal <vitinho2201@gmail.com>
    Co-authored-by: Ezequiel de Oliveira <ezequiel1de1oliveira@gmail.com>

--- src/Screens/WeekSchedule.js
+++ src/Screens/WeekSchedule.js
@@ -225,9 +225,9 @@ export default class WeekSchedule extends Component {
     Alert.alert(
       'Mudar de Horário',
       this.state.currentSchedule.employee + ', deseja trocar de horario' + '?\n\n ' +
-      this.state.currentSchedule.date + '    ->   ' + (this.state.changeDay.getMonth() + 1) + '/' + this.state.changeDay.getDate() + '/' + this.state.changeDay.getFullYear() + '\n' + this.state.currentSchedule.start_time + ' - ' + this.state.currentSchedule.end_time + '  ->  ' + this.state.changeDay.getHours() + ':' + this.state.changeDay.getMinutes() + ' - ' + this.state.finalDateString.getHours() + ':' + this.state.finalDateString.getMinutes(),
+      this.state.currentSchedule.date + '    ->   ' + (this.state.changeDay.getMonth() + 1).toString().padStart(2 , 0) + '/' + (this.state.changeDay.getDate()).toString().padStart(2,0) + '/' + this.state.changeDay.getFullYear() + '\n' + this.state.currentSchedule.start_time + ' - ' + this.state.currentSchedule.end_time + '  ->  ' + (this.state.changeDay.getHours()).toString().padStart(2 , 0) + ':' + (this.state.changeDay.getMinutes()).toString().padStart(2,0) + ' - ' + (this.state.finalDateString.getHours()).toString().padStart(2,0) + ':' + (this.state.finalDateString.getMinutes()).toString().padStart(2,0),
       [
-        { text: 'Não', onPress: () => { this.handleEndDatePicked(), this.timePickerVisible(false) } },
+        { text: 'Não', onPress: () => { this.timePickerVisible(false),this.hideEndDateTimePicker() } },
         { text: 'Sim', onPress: () => { this.requestChange(); } }
       ],
       { cancelable: true }
