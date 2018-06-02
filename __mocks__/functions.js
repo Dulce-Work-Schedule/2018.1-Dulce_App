d771f5fb7db6027387fb6d803489adc361f62cab
Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
2018-06-02 10:21:33 -0300

    #352 Tirando arquivos desnecessários da pasta __mock__
    Signed-off-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>

--- __mocks__/functions.js
+++
@@ -1,2 +0,0 @@
-
-export const login = jest.fn();
--- __mocks__/posts.js
+++
@@ -1,9 +0,0 @@
-import http from 'axios';
-
-export function fechUserLogin() {
-  return http
-    .post('http://localhost:8086/api/userManager/login')
-    .then((response) => {
-      return response.data.success;
-    });
-}
