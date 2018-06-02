00861a031c3436d10b1db5881724d591d161bd1e
Eliseu Egewarth <eliseuegewarth@gmail.com>
2018-06-02 00:11:09 -0300

    HOTFIX #216 Adicionando clausula para evitar deploy em PRs
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>
    Signed-off-by: João Egewarth <egewarth@gmail.com>
    
    Co-authored-by: João Egewarth <egewarth@gmail.com>

--- utils/run_build_in_branch.sh
+++ utils/run_build_in_branch.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 if [ -z $1 ]; then
 	echo "USAGE: $0 BRANCH"
-	exit 1;
 elif [ "$1" = "$TRAVIS_BRANCH" ]; then
-	exit 0;
+	if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+		exit 0;
+	fi
 else
 		echo "Branch $TRAVIS_BRANCH is different of the branch $1..."
-		exit 1;
 fi
+exit 1;
