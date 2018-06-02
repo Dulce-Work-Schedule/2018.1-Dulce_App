0510d8ee0cce3cff58b754de5cec27400f3d822b
Eliseu Egewarth <eliseuegewarth@gmail.com>
2018-06-02 13:17:16 -0300

    Editing log messages in utils/run_build_in_branch.sh
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>

--- utils/run_build_in_branch.sh
+++ utils/run_build_in_branch.sh
@@ -4,13 +4,13 @@ if [ -z $1 ]; then
 elif [ "$1" = "$TRAVIS_BRANCH" ]; then
 	if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
 		repo=$(git rev-parse --show-toplevel) && \
-		git checkout -qf master
-		echo "Estamos num TRAVIS_PULL_REQUEST? a resposta é @$TRAVIS_PULL_REQUEST@"
+		git checkout -qf $1
+		echo -e "\033[0;31mIniciando build deploy\033[0m"
 		${repo}/utils/run_deploy.sh beta && exit 0;
 	else
-		echo "Não estamos num TRAVIS_PULL_REQUEST? a resposta é @$TRAVIS_PULL_REQUEST@"
+		echo -e "\033[0;31mTRAVIS_PULL_REQUEST: #$TRAVIS_PULL_REQUEST#\033[0m"
 	fi
 else
-		echo "Branch $TRAVIS_BRANCH is different of the branch $1..."
+		echo -e "\033[0;31mBranch $TRAVIS_BRANCH is different of the branch $1...\033[0m"
 fi
 exit 1;
