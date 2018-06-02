687befede247b6beb1fead33114c2e3e9efff166
Eliseu Egewarth <eliseuegewarth@gmail.com>
2018-06-02 09:46:03 -0300

    HOTFIX: #216 Adicionando alguns logs
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>

--- utils/run_build_in_branch.sh
+++ utils/run_build_in_branch.sh
@@ -3,7 +3,10 @@ if [ -z $1 ]; then
 	echo "USAGE: $0 BRANCH"
 elif [ "$1" = "$TRAVIS_BRANCH" ]; then
 	if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-		exit 0;
+		# exit 0;
+		echo "Estamos num TRAVIS_PULL_REQUEST? a resposta é @$TRAVIS_PULL_REQUEST@"
+	else
+		echo "Não estamos num TRAVIS_PULL_REQUEST? a resposta é @$TRAVIS_PULL_REQUEST@"
 	fi
 else
 		echo "Branch $TRAVIS_BRANCH is different of the branch $1..."
