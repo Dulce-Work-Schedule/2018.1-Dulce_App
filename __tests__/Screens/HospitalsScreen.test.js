1218fa54163f821ce10b39f551813f3afd71a3ce
Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
2018-06-03 22:43:56 -0300

    #356 79.54% Testes em 100% da SectorScreen e HospitalScreen
    Signed-off-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
    Signed-off-by: Guilherme Deusdará <guibanci@gmail.com>
    
    Co-authored-by: Guilherme Deusdará <guibanci@gmail.com>

--- __tests__/Screens/HospitalsScreen.test.js
+++ __tests__/Screens/HospitalsScreen.test.js
@@ -4,6 +4,8 @@ import HospitalsScreen from '../../src/Screens/HospitalsScreen';
 import renderer from 'react-test-renderer';
 import Adapter from 'enzyme-adapter-react-16';
 import Enzyme from 'enzyme';
+import {shallow} from 'enzyme';
+import expect from 'expect';
 
 Enzyme.configure({adapter: new Adapter()});
 
@@ -12,4 +14,22 @@ it('renders correctly', () => {
   .create(<HospitalsScreen />)
   .toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+it('should test onPressItem correctly', () => {
+  // This is to test component functions
+  const navigation = {navigate: jest.fn()};
+  let hospitalScreen = renderer
+    .create(
+      <HospitalsScreen
+      navigation={navigation}
+        dispatch={action =>
+          expect(action)
+          .toEqual(navigation.navigate)
+        }
+      />
+    )
+    .getInstance();
+
+  hospitalScreen.onPressItem();
 });
--- __tests__/Screens/SectorsScreen.test.js
+++ __tests__/Screens/SectorsScreen.test.js
@@ -9,3 +9,21 @@ it('renders correctly', () => {
   .toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+it('should test onPressItem correctly', () => {
+  // This is to test component functions
+  const navigation = {navigate: jest.fn()};
+  let sectorScreen = renderer
+    .create(
+      <SectorsScreen
+      navigation={navigation}
+        dispatch={action =>
+          expect(action)
+          .toEqual(navigation.navigate)
+        }
+      />
+    )
+    .getInstance();
+
+  sectorScreen._onPressButton();
+});
