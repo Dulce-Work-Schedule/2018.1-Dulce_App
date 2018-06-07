dc9c0d62432a9209abf4f1c1802f4911b2dfd256
Guilherme Deusdará <guibanci@gmail.com>
2018-06-07 19:21:26 -0300

    #382 conseguindo acessar camera e galeria do celular
    Signed-off-by: Guilherme Deusdará <guibanci@gmail.com>
    Signed-off-by: Beatriz Hanae <beatriz.hanae@gmail.com>
    
    Co-Authored-by: Guilherme Deusdará <guibanci@gmail.com>
    Co-Authored-by: Beatriz Hanae <beatriz.hanae@gmail.com>

--- src/Screens/AbsencesScreen.js
+++ src/Screens/AbsencesScreen.js
@@ -1,77 +1,127 @@
 import React from 'react';
-import {View, TouchableHighlight} from 'react-native';
-import {Image, ImagePicker} from 'react-native-image-picker';
+import {
+  StyleSheet,
+  Text,
+  View,
+  PixelRatio,
+  TouchableOpacity,
+  Image
+} from 'react-native';
 
-// const updateProfile = async (user, dispatch, result) => {
-//   dispatch(isFetching());
-//   if (result) {
-//     const localUri = result.uri;
-//     const filename = localUri.split('/').pop();
-//     const match = /\.(\w+)$/.exec(filename);
-//     const type = match ? image/${match[1]} : 'image';
-//     /*eslint-disable */
-//     const formData = new FormData();
-//     /* eslint-enable */
-//
-//     formData.append('file', { uri: localUri, name: filename, type });
-//
-//     await fetch(`${URL}/profile-picture/`, {
-//       method: 'POST',
-//       body: formData,
-//       headers: {
-//         Authorization: Token ${getToken()},
-//       },
-//     });
-//   }
+import ImagePicker from 'react-native-image-picker';
 
+export default class App extends React.Component {
 
-export default class AbscencesScreen extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      image: {}
+  state = {
+    avatarSource: null,
+    videoSource: null
+  };
+
+  selectPhotoTapped() {
+    const options = {
+      quality: 1.0,
+      maxWidth: 500,
+      maxHeight: 500,
+      storageOptions: {
+        skipBackup: true
+      }
     };
+
+    ImagePicker.showImagePicker(options, (response) => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled photo picker');
+      }
+      else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      }
+      else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      }
+      else {
+        let source = {uri: response.uri};
+
+        // You can also display the image using data:
+        // let source = { uri: 'data:image/jpeg;base64,' + response.data };
+
+        this.setState({
+          avatarSource: source
+        });
+      }
+    });
   }
 
-  pickImage() {
-    ImagePicker.launchImageLibrary({
-      allowsEditing: true,
-      maxWidth: 300,
-      maxHeight: 300
-    },
-    // if (!result.cancelled) {
-    //     ,(response) => {result = response;
-    //     this.setState({profilePicture: result.uri });
-    //   }
-    // }
-  );
+  selectVideoTapped() {
+    const options = {
+      title: 'Video Picker',
+      takePhotoButtonTitle: 'Take Video...',
+      mediaType: 'video',
+      videoQuality: 'medium'
+    };
+
+    ImagePicker.showImagePicker(options, (response) => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled video picker');
+      }
+      else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      }
+      else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      }
+      else {
+        this.setState({
+          videoSource: response.uri
+        });
+      }
+    });
   }
 
   render() {
     return (
-    <View>
-    <TouchableHighlight
-      onPress={this.pickImage}
-      underlayColor='#f5f5f5'
-      style={{
-        backgroundColor: '#FB3353',
-        height: 26,
-        width: 26,
-        marginTop: -10,
-        marginLeft: 100,
-        borderRadius: 26 / 2,
-        alignItems: 'center',
-        justifyContent: 'center'
-      }}
-      >
-      <Image
-        onPress={() => {}}
-        source={'image'}
-        style={{height: 12, width: 12
-        }}
-      />
-      </TouchableHighlight>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={this.selectPhotoTapped.bind(this)}>
+          <View style={[styles.avatar, styles.avatarContainer, {marginBottom: 20}]}>
+          { this.state.avatarSource === null ? <Text>Select a Photo</Text>
+            : <Image style={styles.avatar} source={this.state.avatarSource} />
+          }
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={this.selectVideoTapped.bind(this)}>
+          <View style={[styles.avatar, styles.avatarContainer]}>
+            <Text>Select a Video</Text>
+          </View>
+        </TouchableOpacity>
+
+        { this.state.videoSource &&
+          <Text style={{margin: 8, textAlign: 'center'}}>{this.state.videoSource}</Text>
+        }
       </View>
     );
   }
+
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  avatarContainer: {
+    borderColor: '#9B9B9B',
+    borderWidth: 1 / PixelRatio.get(),
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  avatar: {
+    borderRadius: 75,
+    width: 150,
+    height: 150
+  }
+});
