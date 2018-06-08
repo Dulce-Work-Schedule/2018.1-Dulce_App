4c2d0a71197c2eb8a48bcf4bc72389ce6a055120
Beatriz <beatriz.hanae@gmail.com>
2018-06-08 01:33:24 -0300

    #381 Testando o TextInput
    Signed-off-by: Guilherme Deusdará <guibanci@gmail.com>
    Signed-off-by: Beatriz Hanae <beatriz.hanae@gmail.com>
    
    Co-Authored-by: Guilherme Deusdará <guibanci@gmail.com>
    Co-Authored-by: Beatriz Hanae <beatriz.hanae@gmail.com>

--- __tests__/Screens/VacationScreen.test.js
+++ __tests__/Screens/VacationScreen.test.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import 'react-native';
 import VacationScreen from '../../src/Screens/VacationScreen';
+import {shallow} from 'enzyme';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import renderer from 'react-test-renderer';
 
 require('bezier');
+
+Enzyme.configure({adapter: new Adapter()});
 
 it('renders correctly', () => {
   const tree = renderer
   .create(<VacationScreen />)
   .toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+test('justification file', () => {
+  const wrapper = shallow(<VacationScreen />);
+  const justificationField = wrapper.find('TextInput').at(0);
+  justificationField.simulate('changeText','text');
+  expect(wrapper.state('justification')).toBe('text');
 });
