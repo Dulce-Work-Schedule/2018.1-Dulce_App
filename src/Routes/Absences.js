f71a3fbd232ef51e101ef9c094edfc82d65242c1
Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
2018-06-13 23:12:19 -0300

    #416 Adicionando routes para inicializar o navigator de abono
    Signed-off-by: Vitor Leal <vitinho2201@gmail.com>
    Signed-off-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
    
    Co-authored-by: Vitor Leal <vitinho2201@gmail.com>
    Co-authored-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>

---
+++ src/Routes/Absences.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AbsencesNavigator from '../Navigators/AbsencesNavigator';
+
+class Absences extends React.Component {
+
+  render() {
+    return (
+      <AbsencesNavigator />
+    );
+  }
+}
+
+export default Absences;
