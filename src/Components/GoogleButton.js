24cadf48c404a2b491916d1e1b4e9b4369713193
Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
2018-06-19 19:44:56 -0300

    #471 Adicionando styles no google button
    Signed-off-by: Vitor Leal <vitinho2201@gmail.com>
    Signed-off-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
    
    Co-authored-by: Vitor Leal <vitinho2201@gmail.com>
    Co-authored-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>

--- src/Components/GoogleButton.js
+++ src/Components/GoogleButton.js
@@ -1,27 +1,7 @@
+import {googleButton as styles} from '../styles' ;
 import React from 'react';
 import {Text, Image} from 'react-native';
 import {Container, Content, Button} from 'native-base';
-
-const styles = {
-  container: {
-    height: 45,
-    marginTop: 15,
-    paddingHorizontal: 20,
-    alignSelf: 'center',
-    marginBottom: 0,
-    backgroundColor: '#FFF',
-    width: 280
-  },
-  text: {
-    fontSize: 20,
-    alignSelf: 'center',
-    fontFamily: 'Raleway'
-  },
-  img: {
-    height: 40,
-    resizeMode: 'contain'
-  }
-};
 
 class GoogleButton extends React.Component {
 
