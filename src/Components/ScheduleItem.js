35c1108e020dccf1f33eaf0323b918a26f1e2298
Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
2018-06-19 19:45:59 -0300

    #471 Adicionando styles no Schedule item
    Signed-off-by: Vitor Leal <vitinho2201@gmail.com>
    Signed-off-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
    
    Co-authored-by: Vitor Leal <vitinho2201@gmail.com>
    Co-authored-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>

--- src/Components/ScheduleItem.js
+++ src/Components/ScheduleItem.js
@@ -1,29 +1,19 @@
 import React from 'react';
 import {Text, TouchableHighlight, View} from 'react-native';
-
-const styles = {
-  item: {
-    backgroundColor: 'white',
-    flex: 1,
-    borderRadius: 5,
-    padding: 10,
-    marginRight: 10,
-    marginTop: 17
-  }
-};
+import {scheduleItem as styles} from '../styles' ;
 
 class ScheduleItem extends React.Component {
   render() {
     return (
       <TouchableHighlight
-      underlayColor='#5f4b8b'
-      onPress={() => {this.props.onPress();}}
-      style={styles.item}>
-      <View>
-        <Text>{this.props.item.employee}</Text>
-        <Text>{this.props.item.specialty}</Text>
-        <Text>{this.props.item.start_time} - {this.props.item.end_time}</Text>
-        <Text>{this.props.item.amount_of_hours}</Text>
+        underlayColor='#5f4b8b'
+        onPress={() => {this.props.onPress();}}
+        style={styles.item}>
+        <View>
+          <Text>{this.props.item.employee}</Text>
+          <Text>{this.props.item.specialty}</Text>
+          <Text>{this.props.item.start_time} - {this.props.item.end_time}</Text>
+          <Text>{this.props.item.amount_of_hours}</Text>
         </View>
       </TouchableHighlight>
     );
