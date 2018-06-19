a286fd3cf8d77b628e53e92af60f3d94a44ec4f6
Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
2018-06-19 19:48:51 -0300

    #471 Adicionando o styles no small logo
    Signed-off-by: Vitor Leal <vitinho2201@gmail.com>
    Signed-off-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
    
    Co-authored-by: Vitor Leal <vitinho2201@gmail.com>
    Co-authored-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>

--- src/Components/SmallLogo.js
+++ src/Components/SmallLogo.js
@@ -1,18 +1,7 @@
+import {smallLogo as styles} from '../styles' ;
 import React from 'react';
 import {View, Image} from 'react-native';
 const logo = require('../../assets/img/logo.png');
-
-const styles = {
-  logo: {
-    alignSelf: 'center',
-    marginTop: 45,
-    marginBottom: 30,
-    width: 200,
-    height: 100,
-    resizeMode: 'contain',
-    backgroundColor: 'transparent'
-  }
-};
 
 class SmallLogo extends React.Component {
   render() {
