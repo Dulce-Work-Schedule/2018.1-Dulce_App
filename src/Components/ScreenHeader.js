57764dae489e85e073e1f359c48f6e107604a5fb
Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
2018-06-19 19:46:37 -0300

    #471 Adicionando styles no screen header
    Signed-off-by: Vitor Leal <vitinho2201@gmail.com>
    Signed-off-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
    
    Co-authored-by: Vitor Leal <vitinho2201@gmail.com>
    Co-authored-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>

--- src/Components/ScreenHeader.js
+++ src/Components/ScreenHeader.js
@@ -1,27 +1,19 @@
 import React from 'react';
 import {Header, Body, Title} from 'native-base';
+import {screenHeader as styles} from '../styles' ;
 import {StatusBar} from 'react-native';
-
-const styles = {
-  header: {
-    backgroundColor: '#5f4b8b'
-  },
-  text: {
-    fontFamily: 'Raleway'
-  }
-};
 
 class ScreenHeader extends React.Component {
   render() {
     return (
-        <Header style={styles.header}>
+      <Header style={styles.header}>
         <StatusBar
-            backgroundColor='#5f4b79'
+          backgroundColor='#5f4b79'
         />
-          <Body>
-            <Title style={styles.text} > {this.props.title} </Title>
-          </Body>
-        </Header>
+        <Body>
+          <Title style={styles.text} > {this.props.title} </Title>
+        </Body>
+      </Header>
     );
   }
 }
