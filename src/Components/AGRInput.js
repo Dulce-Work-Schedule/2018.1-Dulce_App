abf147cc651834ad46b551824d018348cd7428c7
Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
2018-06-19 19:43:05 -0300

    #471 importando styles no agr input
    Signed-off-by: Vitor Leal <vitinho2201@gmail.com>
    Signed-off-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
    
    Co-authored-by: Vitor Leal <vitinho2201@gmail.com>
    Co-authored-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>

--- src/Components/AGRInput.js
+++ src/Components/AGRInput.js
@@ -1,24 +1,6 @@
+import {agrImput as styles} from '../styles' ;
 import React from 'react';
 import {Input, Item, Form, Label} from 'native-base';
-
-const styles = {
-  textInput: {
-    height: 50,
-    borderBottomWidth: 1,
-    borderBottomColor: 'grey',
-    marginBottom: 10,
-    width: '100%',
-    alignSelf: 'center',
-    fontFamily: 'Lato'
-  },
-  container: {
-    paddingHorizontal: 30
-  },
-  label: {
-    fontFamily: 'Lato',
-    marginTop: 5
-  }
-};
 
 class AGRInput extends React.Component {
   render() {
