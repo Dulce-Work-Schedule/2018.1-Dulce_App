3492026c3db2389c879b6a02e2f3bd5e27ceda4a
Eliseu Egewarth <eliseuegewarth@gmail.com>
2018-06-21 10:50:02 -0300

    Fix #479 Atualizando caminho da logo
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>

--- utils/atualizar_icones.sh
+++ utils/atualizar_icones.sh
@@ -2,8 +2,8 @@
 
 # sudo apt install inkscape
 root_dir=$(git rev-parse --show-toplevel)
-inkscape -z -e ${root_dir}/android/app/src/main/res/mipmap-hdpi/ic_launcher.png -w 72 -h 72 ${root_dir}/docs/VisualDulce/DulceAppIcon.svg
-inkscape -z -e ${root_dir}/android/app/src/main/res/mipmap-mdpi/ic_launcher.png -w 48 -h 48 ${root_dir}/docs/VisualDulce/DulceAppIcon.svg
-inkscape -z -e ${root_dir}/android/app/src/main/res/mipmap-xhdpi/ic_launcher.png -w 96 -h 96 ${root_dir}/docs/VisualDulce/DulceAppIcon.svg
-inkscape -z -e ${root_dir}/android/app/src/main/res/mipmap-xxhdpi/ic_launcher.png -w 144 -h 144 ${root_dir}/docs/VisualDulce/DulceAppIcon.svg
+inkscape -z -e ${root_dir}/android/app/src/main/res/mipmap-hdpi/ic_launcher.png -w 72 -h 72 ${root_dir}/assets/logos/logo_white_purple.svg
+inkscape -z -e ${root_dir}/android/app/src/main/res/mipmap-mdpi/ic_launcher.png -w 48 -h 48 ${root_dir}/assets/logos/logo_white_purple.svg
+inkscape -z -e ${root_dir}/android/app/src/main/res/mipmap-xhdpi/ic_launcher.png -w 96 -h 96 ${root_dir}/assets/logos/logo_white_purple.svg
+inkscape -z -e ${root_dir}/android/app/src/main/res/mipmap-xxhdpi/ic_launcher.png -w 144 -h 144 ${root_dir}/assets/logos/logo_white_purple.svg
 
