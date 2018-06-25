dbd04ebc71f01403f69952c6222d08763cf58d80
Guilherme Deusdará <guibanci@gmail.com>
2018-06-24 22:22:39 -0300

    #495 conectando listProfile com api
    Signed-off-by: Guilherme Deusdará <guibanci@gmail.com>

--- src/Screens/ListProfile.js
+++ src/Screens/ListProfile.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
-import {View} from 'react-native';
-import {Container, Content, Card, CardItem, Text, Body, List, Fab, Icon} from 'native-base';
+import {View, Alert} from 'react-native';
+import {Container, Content, Card, CardItem, Text, Body, List, Fab, Icon, Spinner} from 'native-base';
 import ScreenHeader from '../Components/ScreenHeader';
 import SideBar from '../Components/SideBar';
 import {listProfile as styles} from './styles' ;
+import store from '../Reducers/store';
+import axios from 'axios';
 
 export default class ListProfile extends Component {
 
@@ -20,8 +22,77 @@ export default class ListProfile extends Component {
           matricula: '160119553',
           setor: 'Fepas Beauty'
         }
-      ]
+      ],
+      profiles: [],
+      loading: true
     };
+  }
+
+  componentDidMount() {
+    /*eslint max-nested-callbacks: ["error", 4]*/
+    const url = 'http://18.231.9.190:8083/api/profile/list?user_id=' + store.getState().currentUser.id;
+    console.log(store.getState().currentUser.id);
+    axios.get(url, {
+      headers: {
+        'Authorization': 'Bearer ' + store.getState().currentUser.token
+      }
+    })
+      .then((response) => {
+        if (response.data === []) {
+          Alert.alert(
+            'Erro',
+            'Não há Perfis criados!');
+        } else {
+
+          console.log(response.data);
+          let item = 0;
+
+          response.data.forEach(async (element) => {
+            let profile = {};
+
+            let url2 = 'http://54.94.162.218:8083/api/hospital/view?hospital_id=' + element.hospital_id;
+            let url3 = 'http://18.231.27.220:8083/api/sector/view?sector_id=' + element.sector_id;
+            await axios.get(url2, {
+              headers: {
+                'Authorization': 'Bearer ' + store.getState().currentUser.token
+              }
+            })
+              .then((response1) => {
+                if (response1.data === []) {
+                  Alert.alert(
+                    'Erro',
+                    'Não há hospitais com esse id!');
+                } else {
+
+                  profile.hospital = response1.data.name;
+                  axios.get(url3, {
+                    headers: {
+                      'Authorization': 'Bearer ' + store.getState().currentUser.token
+                    }
+                  })
+                    .then((response2) => {
+                      if (response2.data === []) {
+                        Alert.alert(
+                          'Erro',
+                          'Não há setores com esse id!');
+                      } else {
+                        profile.registration = element.registration;
+                        profile.sector = response2.data.name;
+                        this.state.profiles = [...this.state.profiles, profile];
+                      }
+                      item++;
+                      if (item === response.data.length) {
+                        this.setState({loading: false});
+                      }
+                    });
+
+                }
+              });
+
+          });
+
+        }
+      });
   }
 
   navigateToNewProfile() {
@@ -41,18 +112,28 @@ export default class ListProfile extends Component {
       </Fab>);
   }
 
+  renderSpinner() {
+    return (
+      <Container>
+        <Content>
+          <Spinner color='#5f4b8b'/>
+        </Content>
+      </Container>
+    );
+  }
+
   renderCard(item) {
     return (
       <Card iconLeft>
         <CardItem style={styles.center} button onPress={() => {}}>
           <Body>
-            <Text style={styles.colorPurple}>{item.name}</Text>
+            <Text style={styles.colorPurple}>{item.hospital}</Text>
             <View style={{flexDirection: 'row',flex: 1}}>
               <Icon type='MaterialCommunityIcons' name='hospital-building'
                 style={{padding: 10, fontSize: 40, color: '#5f4b8b'}}/>
               <View style= {{marginTop: 10}} >
-                <Text style={styles.text} > Setor: <Text style={styles.textPurple}>{item.setor}</Text></Text>
-                <Text style={styles.text} > Matricula: <Text style={styles.textPurple}>{item.matricula}</Text></Text>
+                <Text style={styles.text} > Setor: <Text style={styles.textPurple}>{item.sector}</Text></Text>
+                <Text style={styles.text} > Matricula: <Text style={styles.textPurple}>{item.registration}</Text></Text>
               </View>
             </View>
           </Body>
@@ -70,11 +151,13 @@ export default class ListProfile extends Component {
             <ScreenHeader
               title = 'Perfis'
             />
-            <Content>
-              <List dataArray={this.state.notifications}
-                renderRow={(item) => (
-                  this.renderCard(item))} />
-            </Content>
+            {this.state.loading ? (this.renderSpinner()) : (
+              <Content>
+                <List dataArray={this.state.profiles}
+                  renderRow={(item) => (
+                    this.renderCard(item))} />
+              </Content>
+            )}
           </Container>
           {this.fab()}
         </View>
